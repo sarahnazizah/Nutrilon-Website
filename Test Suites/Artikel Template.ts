2cb436dd4134d304fd21708621488d41b89adecf
sarahnazizah <sarahnur.sna@gmail.com>
2025-07-02 16:49:09 +0700

    Update test suites

--- Test Suites/Artikel Template.ts
+++ Test Suites/Artikel Template.ts
@@ -12,29 +12,6 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>a7006489-7aef-47e1-9e94-1895761fc597</testSuiteGuid>
    <testCaseLink>
-      <guid>925bfc5d-659f-4a16-b22c-b120f02607c3</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
-      <iterationNameVariable>
-         <defaultValue>''</defaultValue>
-         <description></description>
-         <id>505c410b-0c6e-4d88-a207-5ef759fa95a2</id>
-         <masked>false</masked>
-         <name>url</name>
-      </iterationNameVariable>
-      <testCaseId>Test Cases/Example testing</testCaseId>
-      <testDataLink>
-         <combinationType>ONE</combinationType>
-         <id>810ff68c-fea3-481b-9686-03e8fecfcd13</id>
-         <iterationEntity>
-            <iterationType>RANGE</iterationType>
-            <value>1-1</value>
-         </iterationEntity>
-         <testDataId>Data Files/Article Url</testDataId>
-      </testDataLink>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
       <guid>6b8cabb7-f04a-4571-b871-a6ed4933222b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
